34db5480b43001cbecebaefef8044f12082009ae
Robert McMahon <robertmcmahon1337@gmail.com>
2015-03-24 23:01:24 -0400

    committing changes

--- proxy.py
+++ proxy.py
@@ -87,7 +87,7 @@ def hexdump(src,length=16):
     
     for i in xrange(0,len(src),length):
         s = src[i:i+length]
-        hexa = b'  '.join(["%0*X"%(digits,ord(x)) for x in s])
+        hexa = b' '.join(["%0*X"%(digits,ord(x))   for x in s])
         text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
         result.append(b"%04X    %-*s    %s"%(i, length*(digits + 1),hexa,text))
         
